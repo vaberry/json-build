b0cd1f7ce257a936e9699a5240aaf7cb0cabd2ca
vberrypryon <vberry@pryoninc.com>
2023-10-17 23:18:40 -0500

    e

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.20'
+DESCRIPTION = 'JSON file builder'
+LONG_DESCRIPTION = 'A package that allows developers to build and save JSON files quickly.'
+
+# Setting up
 setup(
-  name = 'json-build',
-  packages = [],
-  version = '0.0.18',
-  description = 'Package to help developers build JSON file quickly.',
-  author = 'Vince Berry',
-  author_email = 'vincent.berry11@gmail.com',
-  keywords = ['python', 'json', 'dict', 'config'],
-  classifiers = [],
-  install_requires = []
+    name="json-build",
+    version=VERSION,
+    author="Vince Berry",
+    author_email="vincent.berry11@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'json', 'build', 'config'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
 )
