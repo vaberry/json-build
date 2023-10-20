c9267bc26ba3aa006b3eec2c064ee8d9bafb451e
vberrypryon <vberry@pryoninc.com>
2023-10-19 21:52:31 -0500

    e

--- json_build/build.py
+++ json_build/build.py
@@ -1,110 +1,45 @@
-import json 
+import json
 import os
 
 class JSON_Object:
     def __init__(self, outer=None):
-        self.outer = outer if isinstance(outer, list) else {}
-        self.parents = []
-        self.unique_to_keyword_map = {} 
-        self.child_to_parent_map = {}
-        self.display_dict = {}
-    
+        self.outer = outer if isinstance(outer, list) else None
+        self.master_dict = {}
+        self.master_json = {}
+
     def add_object(self, unique_name, keyword, data, parent=None):
-        '''
-        update the unique_to_keyword_map or raise error if unique_name already exists
-        '''
-        if not unique_name in self.unique_to_keyword_map.keys():
-            self.unique_to_keyword_map[unique_name] = keyword
-            if isinstance(data, dict):
-                self.parents.append(unique_name)
+        unique_name = unique_name.strip()
+        keyword = keyword.strip()
+        if not isinstance(data, dict) and not isinstance(data, list):
+            raise ValueError(f"Data must be of type dict or list, not {type(data)}")
+        if not unique_name in self.master_dict.keys():
+            self.master_dict[unique_name] = {
+                "keyword": keyword,
+                "data": data,
+                "parent": parent,
+                "children": []
+            }
+
             if parent:
-                if parent in self.parents:
-                    if parent in self.unique_to_keyword_map.keys():
-                        self.child_to_parent_map[unique_name] = parent
-                    else:
-                        raise ValueError(f"Parent '{parent}' does not exist")
-                else:
-                    raise TypeError(f"Parent can only be a dict object. Options are: {self.parents}")
-            else:
-                self.child_to_parent_map[unique_name] = None
+                self.master_dict[parent]["children"].append(unique_name)
         else:
-            raise ValueError(f"unique_name '{unique_name}' already exists")
-
-        '''
-        create the list of keys to navigate to the correct place in the display_dict
-        e.g output = ['dictLevel 1', 'dictLevel 2', 'dictLevel 3']
-        '''
-        keys_list = [unique_name, parent] if parent else [unique_name]
-        start_key = parent if parent else unique_name
-        while self.child_to_parent_map.get(start_key) is not None:
-            start_key = self.child_to_parent_map.get(start_key)
-            if start_key is not None:
-                keys_list.append(start_key)
-            else:
-                break
-
-        '''
-        create display dict for json dump
-        '''
-        dict_navigation = keys_list[::-1]
-        current_dict = self.display_dict
-        for key in dict_navigation[:-1]:
-            if key not in current_dict:
-                current_dict[key] = {}
-            current_dict = current_dict[key]
-        current_dict[dict_navigation[-1]] = data
-
-        # def replace_keys_with_map(input_dict, key_map):
-        #     current_dict = input_dict
-        #     if isinstance(current_dict, dict):
-        #         new_dict = {}
-        #         for key, value in current_dict.items():
-        #             new_key = key_map.get(key, key)
-        #             new_dict[new_key] = replace_keys_with_map(value, key_map)
-        #         print("new_dict:", new_dict)
-        #         return new_dict
-        #     elif isinstance(current_dict, list):
-        #         return [replace_keys_with_map(item, key_map) for item in current_dict]
-        #     else:
-        #         return current_dict
-            
-        # self.display_dict = replace_keys_with_map(self.display_dict, self.unique_to_keyword_map)
-
+            raise ValueError(f"Unique name '{unique_name}' already exists")
+        
     def create(self):
-        def replace_keys_with_map(input_dict, key_map):
-            if isinstance(input_dict, dict):
-                new_dict = {}
-                for key, value in input_dict.items():
-                    new_key = key_map.get(key, key)
-                    new_value = replace_keys_with_map(value, key_map)
-                    new_dict[new_key] = new_value
-                return new_dict
-            elif isinstance(input_dict, list):
-                return [replace_keys_with_map(item, key_map) for item in input_dict]
+        for _, value in self.master_dict.items():
+            if value["parent"]:
+                if isinstance(self.master_dict[value["parent"]]["data"], list):
+                    if value["data"] not in self.master_dict[value["parent"]]["data"]:
+                        self.master_dict[value["parent"]]["data"].append(value["data"])
+                else:
+                    if value["data"] not in self.master_dict[value["parent"]]["data"].values():
+                        self.master_dict[value["parent"]]["data"][value["keyword"]] = value["data"]
             else:
-                return input_dict
-        self.display_dict = replace_keys_with_map(self.display_dict, self.unique_to_keyword_map)        
-        return json.dumps(self.display_dict, indent=4)
-
+                self.master_json[value["keyword"]] = value["data"]
+            to_dump = [self.master_json] if isinstance(self.outer, list) else self.master_json            
+        return json.dumps(to_dump, indent=4)
+    
     def save(self, file_name, location_path=None):
-        '''
-        in display_dict, replace current unique name keywords with display name keywords
-        '''
-        def replace_keys_with_map(input_dict, key_map):
-            if isinstance(input_dict, dict):
-                new_dict = {}
-                for key, value in input_dict.items():
-                    new_key = key_map.get(key, key)
-                    new_value = replace_keys_with_map(value, key_map)
-                    new_dict[new_key] = new_value
-                return new_dict
-            elif isinstance(input_dict, list):
-                return [replace_keys_with_map(item, key_map) for item in input_dict]
-            else:
-                return input_dict
-        self.display_dict = replace_keys_with_map(self.display_dict, self.unique_to_keyword_map)
-
-
         if not file_name.endswith('.json'):
             file_name = file_name.split('.')[0] + '.json'
         if location_path and not os.path.exists(location_path):
@@ -113,6 +48,5 @@ class JSON_Object:
         else:
             file_path = file_name
 
-        to_dump = [self.display_dict] if isinstance(self.outer, list) else self.display_dict
         with open(file_path, 'w') as f:
-            json.dump(to_dump, f, indent=4)
+            f.write(self.create())
