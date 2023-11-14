e9db5e0850be1c9a10b468608f8f69c5d183f7d6
vaberry <vincent.berry11@gmail.com>
2023-11-13 22:37:40 -0600

    e

--- json_build/build.py
+++ json_build/build.py
@@ -1,84 +1,132 @@
+from collections import OrderedDict
 import json
 import os
-from collections import OrderedDict
 
 class JSON_Object:
-    def __init__(self, outer=None):
-        self.__outer = outer if isinstance(outer, list) else None
-        self.__master_dict = OrderedDict()
-        self.__master_json = OrderedDict()
-        self.__created = False
-        self.display_dict = self.__master_dict
+    def __init__(self):
+        self.master_data = OrderedDict()
 
-    '''
-    This function updates the object model's __master_dict attr with the data needed to create the JSON object
-    '''
-    def add_object(self, unique_name, keyword, data, parent=None):
-        unique_name = unique_name.strip()
-        keyword = keyword.strip()
-        if not isinstance(data, dict) and not isinstance(data, list):
-            raise ValueError(f"Data must be of type dict or list, not {type(data)}")
-        if not unique_name in self.__master_dict.keys():
-            self.__master_dict[unique_name] = {
-                "keyword": keyword,
-                "data": data,
-                "parent": parent,
-                "children": []
-            }
+    """
+    Takes in a JSON object or location and loads it into the master_data dictionary
+    """
+    def load(self, json_object=None, json_location=None):
+        if not json_location and not json_object:
+            raise Exception('No JSON object or location provided')
+        elif json_object and json_location:
+            raise Exception('Cannot provide both JSON object and location')
+        elif json_object:
+            if isinstance(json_object, dict):
+                data = json_object
+            elif isinstance(json_object, str):
+                try:
+                    data = json.loads(json_object)
+                except:
+                    raise Exception('Invalid JSON object')
+        elif json_location:
+            conditions = [
+                os.path.exists(json_location),
+                os.path.isfile(json_location),
+                json_location.endswith('.json')
+            ]
+            if all(conditions):
+                with open(json_location, 'r') as f:
+                    data = json.load(f)
+            else:
+                raise Exception('Invalid JSON location or not a JSON file')
+            def transform_to_tuple_keys(input_dict):
+                output_dict = {}
+                def traverse_dict(d, keys=()):
+                    for key, value in d.items():
+                        new_keys = keys + (key,)
+                        if isinstance(value, dict) and value:
+                            traverse_dict(value, new_keys)
+                        else:
+                            output_dict[new_keys] = value
+                traverse_dict(input_dict)
+                return output_dict
+            flat_dict = transform_to_tuple_keys(data)
+            for k,v in flat_dict.items():
+                unique_name = '_'.join(k)
+                self.master_data[unique_name] = {}
+                self.master_data[unique_name]['location'] = k
+                self.master_data[unique_name]['keyword'] = k[-1]
+                self.master_data[unique_name]['data'] = v
+
+    """
+    Adds a new entry to the master_data dictionary
+    """
+    def add(self, unique_name, keyword, data, parent=None):
+        unique_name = unique_name.lower().strip()
+        keyword = keyword.lower().strip()
+
+        if not unique_name in self.master_data.keys():
+            self.master_data[unique_name] = {}
 
             if parent:
-                self.__master_dict[parent]["children"].append(unique_name)
+                if not parent in self.master_data.keys():
+                    raise Exception(f'Parent ({parent}) does not exist')
+                else:
+                    parent = parent.lower().strip()
+                    self.master_data[unique_name]['location'] = self.master_data[parent]['location'] + (unique_name,)
+            else:
+                self.master_data[unique_name]['location'] = (unique_name,)            
+            
+            
+            self.master_data[unique_name]['keyword'] = keyword
+            self.master_data[unique_name]['data'] = data
         else:
-            raise ValueError(f"Unique name '{unique_name}' already exists")
+            raise Exception('Unique name already exists')
+
+    """
+    Updates an existing entry in the master_data dictionary
+    """   
+    def update(self, unique_name, data, reset=False):
+        unique_name = unique_name.lower().strip()
+        if unique_name in self.master_data.keys():
+            if isinstance(self.master_data[unique_name]['data'], list):
+                if not isinstance(data, list):
+                    raise Exception('Data must be a list')
+                else:
+                    if reset:
+                        self.master_data[unique_name]['data'] = data
+                    else:
+                        self.master_data[unique_name]['data'].extend(data)
+            elif isinstance(self.master_data[unique_name]['data'], dict):
+                if not isinstance(data, dict):
+                    raise Exception('Data must be a dict')
+                else:
+                    if reset:
+                        self.master_data[unique_name]['data'] = data
+                    else:
+                        self.master_data[unique_name]['data'].update(data)
+            elif isinstance(self.master_data[unique_name]['data'], str):
+                if not isinstance(data, str):
+                    raise Exception('Data must be a str')
+                else:
+                    self.master_data[unique_name]['data'] = data
+            elif isinstance(self.master_data[unique_name]['data'], int):
+                if not isinstance(data, int):
+                    raise Exception('Data must be a int')
+                else:
+                    self.master_data[unique_name]['data'] = data
+        else:
+            raise Exception('Unique name does not exist')
         
-    '''
-    This function creates the object model's __master_json attr (Python dict) and dumps that dict into a JSON object
-    '''
-
-    def update_data(self, unique_name, new_data, reset_data=False):
-        if not isinstance(new_data, dict) and not isinstance(new_data, list):
-            raise ValueError(f"Data must be of type dict or list, not {type(new_data)}")
-        elif unique_name not in self.__master_dict.keys():
-            raise ValueError(f"Unique name '{unique_name}' does not exist")
-        else:
-            if isinstance(new_data, dict):
-                if not isinstance(self.__master_dict[unique_name]["data"], dict):
-                    raise ValueError(f"Data must be of type dict, not {type(self.__master_dict[unique_name]['data'])}")
-                else:
-                    if reset_data:
-                        self.__master_dict[unique_name]["data"] = new_data
-                    else:
-                        self.__master_dict[unique_name]["data"].update(new_data)
-            elif isinstance(new_data, list):
-                if not isinstance(self.__master_dict[unique_name]["data"], list):
-                    raise ValueError(f"Data must be of type list, not {type(self.__master_dict[unique_name]['data'])}")
-                else:
-                    if reset_data:
-                        self.__master_dict[unique_name]["data"] = new_data
-                    else:
-                        self.__master_dict[unique_name]["data"].extend(new_data)
-
-
+    """
+    Uses the master_data dictionary to create a JSON object
+    """
     def create(self, collapse=False, trim=False):
-        if not self.__created: # This ensures that the master_json dict creation only happens once during the script run
-            self.__created = True
-            for _, value in self.__master_dict.items():
-                if value["parent"]:
-                    # How to handle if parent of object is a list
-                    if isinstance(self.__master_dict[value["parent"]]["data"], list):
-                        if value["data"] not in self.__master_dict[value["parent"]]["data"]: # If dict object is not already in the list (no dupes)
-                            self.__master_dict[value["parent"]]["data"].append(value["data"])
+        new_dict = {}
+        current_dict = new_dict
+        for _,v in self.master_data.items():
+            for item in v['location'][:-1]:
+                if not item in current_dict.keys():
+                    current_dict[item] = {}
+                current_dict = current_dict[item]
+            current_dict[v['location'][-1]] = v['data']
+            current_dict = new_dict
+        to_dump = new_dict
 
-                    # How to handle if parent of object is a dict
-                    else:
-                        if value["keyword"] not in self.__master_dict[value["parent"]]["data"].keys():
-                            self.__master_dict[value["parent"]]["data"][value["keyword"]] = value["data"]
-                        else:
-                            raise ValueError(f"You are trying to add a duplicate keyword ('{value['keyword']}'). Please change one of the keywords.")
-                else:
-                    self.__master_json[value["keyword"]] = value["data"]
-        to_dump = [self.__master_json] if isinstance(self.__outer, list) else self.__master_json
-        
         if trim:
             def remove_null_empty_values(data):
                 if isinstance(data, dict):
@@ -107,26 +155,24 @@ class JSON_Object:
                     return data
                 else:
                     return data
-                
             to_dump = remove_null_empty_values(to_dump)
-        
+
         if collapse:
             return json.dumps(to_dump)
         else:    
             return json.dumps(to_dump, indent=4)
-    
-
-    '''
-    This function calls the create() method, then saves the returned JSON object to a file
-    '''
-    def save(self, file_name, location_path=None):
-        if not file_name.endswith('.json'):
-            file_name = file_name.split('.')[0] + '.json'
+        
+    """
+    Calls the create() function to create a JSON object, then the JSON object to a file locally
+    """
+    def save(self, filename, location_path=None, trim=False):
+        if not filename.endswith('.json'):
+            filename = filename.split('.')[0] + '.json'
         if location_path and not os.path.exists(location_path):
             os.makedirs(location_path)        
-            file_path = os.path.join(location_path, file_name)
+            file_path = os.path.join(location_path, filename)
         else:
-            file_path = file_name
+            file_path = filename
 
         with open(file_path, 'w') as f:
-            f.write(self.create())
+            f.write(self.create(trim=trim))
